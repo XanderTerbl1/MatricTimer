022a4a58d099bdb391f625d57a23d553fcc8ca1b
XanderTerbl1 <xanderterbl268@gmail.com>
2017-08-21 20:38:42 +0200

    Asset Commit
    For Master

---
+++ MatricTimer/MatricTimer/Assets/Scripts/FormController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FormController : MonoBehaviour
+{
+
+    [Header("Main Forms")]
+    public List<GameObject> MainForms;
+
+    void Start()
+    {
+        //Onstart Load the Welcome Screen first...
+        for (int i = 0; i < MainForms.Count; i++)
+        {
+            MainForms[i].SetActive(false);
+            MainForms[i].transform.position = MainForms[0].transform.position;
+        }
+
+        MainForms[0].SetActive(true);//Activate only the main form.
+
+    }
+
+
+    void Update() {
+
+    }
+
+
+   public void ChangeActiveForm(int NewActiveForm)
+    {
+        for (int i = 0; i < MainForms.Count; i++)
+        {
+            MainForms[i].SetActive(false);
+        }
+
+       
+    }
+}
---
+++ MatricTimer/MatricTimer/Assets/Scripts/UI_Controller.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Controller : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject BG_Shader;
+    [SerializeField]
+    private GameObject SlideMenu;
+    //
+    [SerializeField]
+    private GameObject TopLeftPoint;
+    [HideInInspector]
+    public Vector3 StartPoint_SlideMenu;  //Start and End point are used as xref's
+    [HideInInspector]
+    public Vector3 EndPoint_SlideMenu;  //Start and End point are used as xref's
+
+    void Awake ()
+    {
+       
+
+        Vector3 SlideStart;
+        SlideStart.x = TopLeftPoint.transform.position.x;
+        SlideStart.y = SlideMenu.transform.position.y;
+        SlideStart.z = SlideMenu.transform.position.z;
+
+        StartPoint_SlideMenu = SlideStart;
+
+    
+        Vector3 SlideEnd;
+        SlideEnd.x = SlideMenu.transform.position.x;
+        SlideEnd.y = SlideMenu.transform.position.y;
+        SlideEnd.z = SlideMenu.transform.position.z;
+        EndPoint_SlideMenu = SlideEnd;
+
+
+        TweenTransforms TweenTrans = SlideMenu.GetComponent<TweenTransforms>();
+        TweenTrans.startingVector = GetSliderStart();
+        TweenTrans.endVector = GetSliderEnd();
+
+        Debug.Log(SlideMenu.transform.localPosition);
+
+    }
+
+    public Vector3 GetSliderStart()
+    {
+      
+        return StartPoint_SlideMenu;
+    }
+
+    public Vector3 GetSliderEnd()
+    {
+        return EndPoint_SlideMenu;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+
+		
+	}
+
+    public void ShaderState(bool ShaderState)
+    {
+        
+
+        if (ShaderState)
+        {
+            BG_Shader.SetActive(true);
+            TweenTransforms TweenTrans = SlideMenu.GetComponent<TweenTransforms>();
+            TweenTrans.startingVector = GetSliderStart();
+            TweenTrans.endVector = GetSliderEnd();
+        }
+        else {
+            BG_Shader.SetActive(false);
+            TweenTransforms TweenTrans = SlideMenu.GetComponent<TweenTransforms>();
+            TweenTrans.startingVector = GetSliderEnd();
+            TweenTrans.endVector = GetSliderStart();
+        }
+    }
+}
