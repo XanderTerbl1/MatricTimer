d8bb8846896a861194cf084c38e34515414eae44
XanderTerbl1 <xanderterbl268@gmail.com>
2017-09-19 21:51:08 +0200

    FeatCommit
    UpdateGui + Workflow

---
+++ MatricTimer/MatricTimer/Assets/AnimationController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour {
+
+
+
+    public  enum TPositionState {psStart, psHorz, psSingles};
+    public enum TSingle {sDays , sHours , sMinutes, sSeconds ,sNone};
+
+    TSingle TheSingle = TSingle.sNone;
+    TPositionState TheState;
+
+    public Animator DoubleTimeAnim;
+    public GameObject Circle;
+    float rotation = 0;
+
+
+    void Start ()
+    {
+        TheState = TPositionState.psHorz;
+	}
+
+    void Update()
+    {
+
+    }
+	
+
+
+    public void doAnimate()
+    {
+
+        if (TheState == TPositionState.psStart)
+        {
+            //Move to Horz
+            DoubleTimeAnim.SetTrigger("toHorz");
+            TheState = TPositionState.psHorz;
+        }
+        else if (TheState == TPositionState.psHorz)
+        {
+            DoubleTimeAnim.SetTrigger("toVertz");
+            TheState = TPositionState.psStart;
+        }
+        else
+        {
+            //Change the Singles Animation
+        }
+    }
+}
